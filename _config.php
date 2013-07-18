44e737877c309c93fc84ea848cde12bdf246e134
pett <pett@7b40dde9-fc51-43d4-b609-63a66f0a0cee>
2013-07-18 00:16:28 +0000

    UPGRADE
    git-svn-id: http://sunny.svnrepository.com/svn/sunny-side-up-general/campaignmonitor/trunk@4902 7b40dde9-fc51-43d4-b609-63a66f0a0cee

--- _config.php
+++ _config.php
@@ -10,9 +10,5 @@
 
 //copy the lines between the START AND END line to your /mysite/_config.php file and choose the right settings
 //===================---------------- START campaignmonitor MODULE ----------------===================
-// -- MUST HAVE --
-//CampaignMonitorWrapper::set_campaign_monitor_url("http://yourcompany.createsend.com/");
-//CampaignMonitorWrapper::set_api_key("xxxxxxxxxxxxxxxxxxxx"); (see http://yourcompany.createsend.com/admin/account/)
-//CampaignMonitorWrapper::set_client_ID("xxxxxxxxxxxxxxxxxxxx");
-//Object::add_extension('Member', 'CampaignMonitorMemberDOD');
+
 //===================---------------- END campaignmonitor MODULE ----------------===================
