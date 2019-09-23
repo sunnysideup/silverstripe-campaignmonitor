a61a29b0378823efb2892cfeed8c044cc9c732fa
thiemen <nt@sunnysideup.co.nz>
2019-09-23 19:37:09 +1200

    MAJOR: upgrade to new version of Silverstripe - step: Add the table name

--- code/CampaignMonitorSignupPage.php
+++ code/CampaignMonitorSignupPage.php
@@ -40,6 +40,20 @@ class CampaignMonitorSignupPage extends Page
      *
      * @inherited
      */
+
+/**
+  * ### @@@@ START REPLACEMENT @@@@ ###
+  * OLD: private static $db (case sensitive)
+  * NEW: 
+    private static $table_name = '[SEARCH_REPLACE_CLASS_NAME_GOES_HERE]';
+
+    private static $db (COMPLEX)
+  * EXP: Check that is class indeed extends DataObject and that it is not a data-extension!
+  * ### @@@@ STOP REPLACEMENT @@@@ ###
+  */
+    
+    private static $table_name = 'CampaignMonitorSignupPage';
+
     private static $db = array(
         'ListID' => 'Varchar(32)',
 
