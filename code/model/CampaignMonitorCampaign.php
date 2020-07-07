56fa6d5f5787c65e8780039ff920554285c247a2
shane <shane.watters.nz@gmail.com>
2020-07-07 12:39:12 +1200

    MAJOR: upgrade to new version of Silverstripe - step: Add the table name

--- code/model/CampaignMonitorCampaign.php
+++ code/model/CampaignMonitorCampaign.php
@@ -29,6 +29,20 @@ class CampaignMonitorCampaign extends DataObject
      */
     private static $default_template = "CampaignMonitorCampaign";
     
+
+/**
+  * ### @@@@ START REPLACEMENT @@@@ ###
+  * OLD: private static $db (case sensitive)
+  * NEW: 
+    private static $table_name = '[SEARCH_REPLACE_CLASS_NAME_GOES_HERE]';
+
+    private static $db (COMPLEX)
+  * EXP: Check that is class indeed extends DataObject and that it is not a data-extension!
+  * ### @@@@ STOP REPLACEMENT @@@@ ###
+  */
+    
+    private static $table_name = 'CampaignMonitorCampaign';
+
     private static $db = array(
         "HasBeenSent" => "Boolean",
         "MessageFromNewsletterServer" => "Text",
